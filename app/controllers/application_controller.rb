2882c35d73523d32723f1815d286dea90d635af7
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-27 03:29:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < Sinatra::Base
   
   post '/recipes' do 
     @recipe = Recipes.create(params)
-    redirect '/recipies/#{params'
+    redirect '/recipies/#{'
   end 
 end
